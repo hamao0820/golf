635225400a9e6e5fb1b1e9a3cb97fa2d302d2b34
浜田　俊祐 <hamashun0820@icloud.com>
2023-07-08 14:40:50 +0900

    dom要素をViewクラスの中で作るようにした

--- src/ts/ui/view.ts
+++ src/ts/ui/view.ts
@@ -4,13 +4,30 @@ import Model from './model';
 class View {
     #controller: Controller;
     #model: Model;
+
+    #startButton: HTMLElement;
+    #game: HTMLElement;
+    #layout: HTMLElement;
+    #stock: HTMLElement;
+    #lead: HTMLElement;
+
     constructor() {
         this.#controller = new Controller();
         this.#model = new Model();
-        
-        const startButton = document.getElementById('startButton');
-        if (!startButton) throw Error;
-        startButton.addEventListener('click', this.#controller.handleStartButtonClicked);
+
+        this.#game = document.createElement('div');
+        this.#layout = document.createElement('div');
+        this.#stock = document.createElement('div');
+        this.#lead = document.createElement('div');
+        this.#game.appendChild(this.#layout);
+        this.#game.appendChild(this.#stock);
+        this.#game.appendChild(this.#lead);
+        document.body.appendChild(this.#game);
+
+        this.#startButton = document.createElement('button');
+        this.#startButton.innerText = 'start';
+        this.#startButton.addEventListener('click', this.#controller.handleStartButtonClicked);
+        document.body.appendChild(this.#startButton);
         return;
     }
 }
